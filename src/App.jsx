122bcf3362481c78f76fcb39f284c1e1ea5e142d
ehsan <ehsansaraee6570@yahoo.com>
2022-03-03 16:59:52 +0330

    routing app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Fade } from 'react-awesome-reveal';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Footer from './containers/Footer';
+import Header from './containers/Header';
+import Home from './containers/Home';
+import { GlobalStyles } from './globalStyles';
+
 const App = () => {
    return (
-      <>
-         <h1>STARBUCKS</h1>
-      </>
+      <BrowserRouter>
+         <GlobalStyles />
+         <Switch>
+            <Route exact path="/">
+               <Header />
+               <Home />
+               <Fade>
+                  <Footer />
+               </Fade>
+            </Route>
+         </Switch>
+      </BrowserRouter>
    );
 };
 
